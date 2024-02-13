6da166c4b3fddb822ef72ad5ff9a83c9a340571c
Demetrius81 <shizimaniak@gmail.com>
2024-02-13 11:32:09 +0300

    feat: seminar is done

---
+++ NetworkLessons/Lesson1.Seminar.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+namespace Lesson1.Seminar.Client;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        TcpClient tcpClient = new TcpClient();
+        Thread.Sleep(1000);
+        tcpClient.Connect("127.0.0.1", 5000);
+        Console.ReadKey();
+    }
+}
